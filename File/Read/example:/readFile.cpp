4d7431ee7613cb4ad28dd7819e5f2b597f4a2a37
Source Graph A.I <117184625+UniInc@users.noreply.github.com>
2024-05-14 02:27:39 -0500

    Create readFile.cpp

---
+++ File/Read/example:/readFile.cpp
@@ -0,0 +1,44 @@
+// file
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+// getHome
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+
+using namespace std;
+
+string getHome(){
+    struct passwd *pw = getpwuid(getuid());
+
+    const char *homedir = pw->pw_dir;
+    return homedir;
+}
+
+void readFile(){
+    string file = getHome() + "/Desktop/num";
+
+    ifstream fin;
+    fin.open(file);
+    if (!fin){
+        cout << "Error";
+        exit(1);
+    }
+    int val;
+
+    while (!fin.eof())
+    {
+        fin >> val;
+        if( fin.eof() ) break;
+        cout << val << " " << endl;
+    }
+    
+
+    fin.close();
+}
+
+
+int main(){
+    return 0;
+}
